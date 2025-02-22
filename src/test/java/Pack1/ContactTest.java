8062456297b83b85bc74f7d98a60a0d3753cf710
aaratiInamdar <arti.sanas3555@gmail.com>
2025-02-22 12:05:15 +0530

    updated

---
+++ src/test/java/Pack1/ContactTest.java
@@ -0,0 +1,28 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	
+	@Test
+	public void createContactTest()
+	{
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String UN = System.getProperty("username");
+		String PWD = System.getProperty("pwd");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(UN);
+		System.out.println(PWD);
+		System.out.println("execute createContactTest");
+	}
+	
+	@Test
+	public void updateContactTest()
+	{
+		System.out.println("execute createContactTest");
+	}
+}
